8db6479c3f63a7dfe93b318c8a0ca13f15c82f0e
Vannjames <120346985+Vannjames@users.noreply.github.com>
2022-12-19 11:32:06 +0700

    Lesson 05

---
+++ 03_CheChem/05_ex01_variable.php
@@ -0,0 +1,12 @@
+<?php
+    //Declearng Variable
+    $txt = "Hello World";
+    $num = 123456789;
+    $colors = array("Red","Green","Blue");
+
+    //បង្ហាតម្លៃក្នងអថេរខាងលើ
+    echo $txt;
+    echo "<br> $num";
+    echo "<br>";
+    echo $colors[1]; 
+?>
