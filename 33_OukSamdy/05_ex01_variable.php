a6e185c7a9ae5954b5010e5bd8bd9295c768e101
Samdy_ouk <oukkh118@gmail.com>
2022-12-26 09:42:05 +0700

    Upload New project

--- 33_OukSamdy/05_ex01_variable.php
+++ 33_OukSamdy/05_ex01_variable.php
@@ -1,27 +1,11 @@
-<?php 
-   <?php
+<?php
    //declear variables
    $txt = "Hello World";
    $num = 123456789;
    $colors = array("Red", "Green", "Blue");
 
    //Display Variable
-   echo $txt;
-   echo "<br> $num";
-   echo $colors[1];
-
-
-
-
-
-
-
-?>
-
-
-
-
-
-
-
+     echo $txt;
+     echo "<br> $num";
+     echo $colors[1];
 ?>
