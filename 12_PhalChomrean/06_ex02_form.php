04e84b8c267d1587bb4e397f5d491098ee32f4a6
chomrean <phalchomrean@gmail.com>
2022-12-26 20:55:15 +0700

    update file

---
+++ 12_PhalChomrean/06_ex02_form.php
@@ -0,0 +1,47 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        @font-face {
+            font-family: battambong;
+            src: url(KHMEROSBATTAMBANG.TTF);
+        }
+        body{
+             font-family:battambong;
+        }
+    </style>
+</head>
+<body>
+   <div>
+    <form action="06_ex02_form.php" method="post">
+       <h3>បញ្ជូលព័ត៍មាន</h3>
+        <div>
+            <label for="firstname">នាមត្រកូល</label><br>
+            <input type="text" id="firstname" name="firstname" value="នាមត្រកូល"><br>
+            <label for="lastname">នាមខ្លូន</label><br>
+            <input type="text" id="lastname" name="lastname" value="នាមខ្លូន"><br>
+        </div>
+        <div>
+            <label>ភេទ</label><br>
+            <input type="radio" id="male" name="gender" value="male">
+            <label for="male">ប្រុស</label><br>
+            <input type="radio" id="female" name="gender" value="female">
+            <label for="female">ស្រី</label>
+        </div>
+        <div>
+            <label for="color">ជ្រើសរើសពណ៍</label><br>
+            <select name="color" id="color" width="200px">
+                <option value="color">red</option>
+            </select>
+        </div>
+        <div>
+            <input type="submit" value="DisplayData">
+        </div>
+    </form>
+   </div>
+</body>
+</html> -->
