2e7577036f469235345ef0a77c3bc80b5c2de722
Reaksa <119832732+reaksa-meng@users.noreply.github.com>
2022-12-26 22:32:37 +0700

    New Upload Lession file

---
+++ 19_MengReaksa/CodeLession/05_EchoStatement.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Application</title>
+</head>
+<body>
+    <?php
+
+        // Displaying HTML Code 
+
+        echo " <h4> This is a simple heading. </h4> ";
+
+        echo " <h4> This is heading with style. </h4> ";
+     
+    ?>
+        
+</body>
+</html>
