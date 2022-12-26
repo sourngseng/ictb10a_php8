03a99e28477e57f114160712509c0ba53aa7645f
Hak Hai Hero <haihero861@gmail.com>
2022-12-26 10:45:07 +0700

    lesson6

--- 28_hakhai/06_ex01_data _type_int.php
+++ 28_hakhai/06_ex01_data _type_int.php
@@ -2,9 +2,16 @@
 <html>
 <body>
 
-<?php  
-$x = 5985;
-var_dump($x);
+<?php   
+    $dec1 = 34;  
+    $oct1 = 0243;  
+    $hexa1 = 0x45;
+    $a=0.123;
+    var_dump($a);
+    var_dump($dec1); 
+    echo "Decimal number: " .$dec1. "</br>";  
+    echo "Octal number: " .$oct1. "</br>";  
+    echo "HexaDecimal number: " .$hexa1. "</br>";  
 ?>  
 
 </body>
