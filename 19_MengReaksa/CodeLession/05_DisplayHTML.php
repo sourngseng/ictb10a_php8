cd037d8fc8bebecd3828f4e500b69b435caa39d3
Reaksa <119832732+reaksa-meng@users.noreply.github.com>
2022-12-27 07:30:38 +0700

    New Lession

--- 19_MengReaksa/CodeLession/05_DisplayHTML.php
+++ 19_MengReaksa/CodeLession/05_DisplayHTML.php
@@ -13,7 +13,7 @@
 
         echo " <h4> This is a simple heading. </h4> ";
 
-        echo " <h4 ' style = color:red; '> This is heading with style. </h4> ";
+        echo " <h4 style='color:red;'> This is heading with style. </h4> ";
      
     ?>
         
