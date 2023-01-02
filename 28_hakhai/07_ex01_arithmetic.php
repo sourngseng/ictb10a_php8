143783167e3d4c05511caf2c2f1d16bfdb9bf9c1
Hak Hai Hero <haihero861@gmail.com>
2023-01-02 10:04:38 +0700

    lesson 07 arithmetic and assignment

---
+++ 28_hakhai/07_ex01_arithmetic.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 10;  
+$y = 6;
+
+echo $x + $y;
+echo"<br>";
+echo $x * $y;
+echo"<br>";
+echo $x - $y;
+echo"<br>";
+echo $x / $y;
+echo"<br>";
+echo $x % $y;
+echo"<br>";
+?>  
+
+</body>
+</html>
