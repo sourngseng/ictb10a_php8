d27d7543ab5b231e47a5c4db562e94feb298741a
Hak Hai Hero <haihero861@gmail.com>
2023-01-02 09:09:44 +0700

    lesson07

--- 28_hakhai/06_ex04_form1.php
+++ 28_hakhai/06_ex04_form1.php
@@ -7,16 +7,18 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="login.php" method="post">   
+    <form action="06_ex04_form1.php" method="post">   
     <table>   
-    <tr><td>Name:</td><td> <input type="text" name="name"/></td></tr>  
-    <tr><td>Password:</td><td> <input type="password" name="password"/></td></tr>   
+    <tr><td>Name:</td><td> 
+        <input type="text" name="name"/></td></tr>  
+    <tr><td>Password:</td><td> 
+        <input type="password" name="password"/></td></tr>   
     <tr><td colspan="2"><input type="submit" value="login"/>  </td></tr>  
     </table>  
     </form>   
     <?php  
-        $name=$_POST["name"];
-        $password=$_POST["password"];
+        @$name=$_POST["name"];
+        @$password=$_POST["password"];
         echo "Welcome: $name, your password is: $password";  
     ?>      
 </body>
