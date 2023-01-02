7be1d431d8d5c4b426127463f678fa42e3ae40bf
Nang Vanneth <nvanneth12@gmail.com>
2023-01-02 11:06:50 +0700

    lesson07

--- 06_NangVanneth/lesson07/07_ex01_arithmetic.php
+++ 06_NangVanneth/lesson07/07_ex01_arithmetic.php
@@ -2,24 +2,24 @@
     $x = 10;
     $y = 4;
 
-    echo("Result of $x + $y =" . $x + $y); // 0utputs: 14
+    echo("Result of $x + $y =" . $x + $y);
     echo "<br>";
 
-    echo("Result of $x - $y =" .$x - $y); // 0utputs: 6
+    echo("Result of $x - $y =" .$x - $y);
     echo "<br>";
 
-    echo("Result of $x x $y =" .$x * $y); // 0utputs: 40
+    echo("Result of $x x $y =" .$x * $y); 
     echo "<br>";
 
-    echo("Result of $x / $y =" . $x / $y); // 0utputs: 2.5
+    echo("Result of $x / $y =" . $x / $y); 
     echo "<br>";
 
-    echo("Result of $x % $y =" .$x % $y); // 0utputs: 2
+    echo("Result of $x % $y =" .$x % $y); 
 
     $result=2*($x+$y)/3;
     echo "<br>";
 
-     echo("កន្សោមប្រមាណវិធី =" .$result); // 0utputs: 9.3333333333333
+     echo("កន្សោមប្រមាណវិធី =" .$result); 
 
 
 ?>
