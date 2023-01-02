0c107b58de1bfcf94c71e5ae24334a4b080a5d26
SENG Sourng <sengsourng@istc.edu.kh>
2023-01-03 01:31:02 +0700

    Update Lesson 5

--- 01_SengSourng/lesson05/05_ex02_html5.php
+++ 01_SengSourng/lesson05/05_ex02_html5.php
@@ -17,5 +17,19 @@
         echo "<h1>Hello Heading 1!</h1>";        
         echo "<img src='../images/img_avatar1.png' width='150'>";
     ?>
+
+    <?php
+        print "<h2>Hello , Sourng!</h2>";
+        print "<h1>Heading with Print()!</h1>";        
+        print "<img src='../images/img_avatar1.png' width='150'>";
+    ?>
+
+    <?php
+        // Displaying HTML code
+        print "<h4>This is a simple heading.</h4>";
+        print "<h4 style='color: red;'>
+        This is heading with style.</h4>";
+    ?>
+
 </body>
 </html>
