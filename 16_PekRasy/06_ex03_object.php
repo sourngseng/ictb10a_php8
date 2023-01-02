9edc4069b052721a0d9ec5112cc129916156be00
pekrasy123 <119833090+pekrasy123@users.noreply.github.com>
2023-01-02 08:37:25 +0700

    lesson6

---
+++ 16_PekRasy/06_ex03_object.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tesing DataType with From</title>
+</head>
+<body>
+<?php 
+    @$fName=$_POST['firstName'];
+    @$lName=$_POST['lastName'];
+    ?>
+    <form actoin="06_ex01_form.php" method="post">
+        <div style="margin-bottom: 15px;">
+             <label for="fname">First Name:</label>
+             <input type="text" id="fname" name="firstName">
+        </div>
+        <div style="margin-bottom: 15px;">
+            <label for="lname">last Name:</label>
+            <input type="text" id="lname" name="lastName">
+        </div>
+        <input style="margin-left: 120px;" type="submit" value="Save";>
+    </form>
+    <h3>
+        <?php 
+        echo "ឈ្មោះរបស់អ្នកគឺ​ : " . $fName . " " . $lName;
+        ?>
+    </h3>
+</body>
+</html>
