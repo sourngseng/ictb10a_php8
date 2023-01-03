7c5204d78473329dda7287d1c73f6f5b3e91facb
SAO vandy <119833324+saovandy@users.noreply.github.com>
2023-01-03 09:28:10 +0700

    lession3

---
+++ 26_SaoVandy/lession03/04_C _Sensitivty in php.php
@@ -0,0 +1,8 @@
+<?php
+// assign value to variable 
+$color ="blue";
+//Try to print variable value 
+echo "The color of the sky is " . $color ."<br>";
+echo "The color of the sky is " . $color ."<br>";
+echo "The color of the sky is " . $color ."<br>";
+?>
