68ec2e4c21651e9b5e41da600e3044c0f1f87170
Reaksa <119832732+reaksa-meng@users.noreply.github.com>
2023-01-04 14:07:31 +0700

    Lession 09

--- 19_MengReaksa/All Lession/Lession 09 Array/09_ex01_Indexed.php
+++ 19_MengReaksa/All Lession/Lession 09 Array/09_ex01_Indexed.php
@@ -4,18 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> if elseif else Statement </title>
+    <title> Indexed Array </title>
 </head>
-<body> 
-    <!-- Not ready  -->
+<body>
     <?php
     
-    if ($age < 18){
-        echo " Child! ";
-    }else{
-        echo " Adult! ";
-    equal to 18
-    }
+    // Define an indexed Array 
+    $colors = array("Red", "Green", "Blue");
+    echo $colors[0];
+    echo "<br>";
+
+    echo $colors[1];
+    echo "<br>";
+
+    echo $colors[2];
     
     ?>
 </body>
---
+++ 19_MengReaksa/All Lession/Lession 09 Array/09_ex02_Associative.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Associative Array </title>
+</head>
+<body>
+    <?php
+    
+    // Define an Associative array 
+    $age = array("Peter"=>22, "Clark"=>32, "Jonh"=>28);
+    echo $age["Peter"];
+    print "<br>";
+
+    echo $age["Clark"];
+    print "<br>";
+
+    echo $age["Jonh"];
+    print "<br>";
+    
+    ?>
+</body>
+</html>
