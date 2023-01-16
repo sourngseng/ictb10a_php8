6b824319220519cd74a7d40cd9f3c0cca1c4becf
soumvimean <120348944+soumvimean@users.noreply.github.com>
2023-01-16 11:19:46 +0700

    soum vimean

--- 25_SoumVimean/lesson08/08_ex01_if_statement.php
+++ 25_SoumVimean/lesson08/08_ex01_if_statement.php
@@ -4,13 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>testing Datatype with form</title>
+    <title>Document</title>
 </head>
 <body>
-  
-
-
-
+    <?php
+        $d = "Fri";
+    if($d=="Fri"){
+    echo "Have a nice Weekend!"  ;
+    }
+        echo "<br>";
+        $t = date("H");
+    if($t<20){
+    echo "Have a good day!";
+}
+?>
 
 </body>
 </html>
