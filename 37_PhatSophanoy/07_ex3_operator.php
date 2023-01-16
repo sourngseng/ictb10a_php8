5af113c86fe18a74bc9af881553b2ce91000d798
PHAT Sophanoy <119833505+phatsophanoy@users.noreply.github.com>
2023-01-16 09:17:11 +0700

    lession8

---
+++ 37_PhatSophanoy/07_ex3_operator.php
@@ -0,0 +1,19 @@
+<?php
+    $x=25;
+    $y=35;
+    $z="25";
+    var_dump($x==$z); //outputs: boolean true
+    var_dump($x===$z); //outputs: boolean false
+    var_dump($x !==$y); //outputs: boolean true
+    var_dump($x !==$z); //outputs: boolean false
+    var_dump($x<$y); //outputs: boolean true
+    var_dump($x>$y); //outputs: boolean false
+    var_dump($x<=$y); //outputs: boolean true
+    var_dump($x>=$y); //outputs: boolean false
+    
+    
+    
+    
+    
+
+?>
