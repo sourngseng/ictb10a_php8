06ede7b586fc5fcba6c3c2009e64e1d535c0e3b1
Nang Vanneth <nvanneth12@gmail.com>
2023-01-16 11:25:22 +0700

    lesson8 upload

---
+++ 06_NangVanneth/lesson07/lesson8/08_ex06_ternary.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Using Ternary Operator</title>
+</head>
+<body>
+<?php
+$num1=$num2=null;
+    if(isset($_POST['submit']))
+    {
+        $num1 = isset($_POST['num1']) ? $_POST['num1'] : null;
+        $num2 = isset($_POST['num2']) ? $_POST['num2'] : null;
+        $max_value=$num1>$num2?$num1:$num2;
+    }
+?> 
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <p>
+        <label>លេខទី១ : <input type="text" name="num1" value="<?php echo $num1?:null; ?>"></label>
+    </p>
+    <p>
+        <label>លេខទី២ : <input type="text" name="num2" value="<?php echo $num2?:null; ?>"></label>
+    </p>
+    <p>
+        <input type="submit" name="submit" value="Find Max">
+    </p>
+</form>
+
+<?php
+    if(isset($_POST['submit']))
+    {
+        echo "<p>Your Max Number is : $max_value</p>";            
+    }
+?>
+</body>
+</html>
