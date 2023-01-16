e2c8a12d0a4da5c7c6d76892ddb6e1b86dabac6f
rossreynuth <121003274+rossreynuth@users.noreply.github.com>
2023-01-16 22:51:00 +0700

    hello

---
+++ 20_ros_sreynuth/Lesson8/08_ex01_test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container col-md-4 mt-5 card">
+    <form action="<?= $_SEVER['PHP_SELF']" method="post">
+    <input type="text" name="first_name" id="first_name">
+    
+    
+    
+    
+    </form>
+    </div>
+</body>
+</html>
