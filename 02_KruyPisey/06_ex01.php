1773c4a7f5d27a03e4d04fd329775015411460fd
kruypisey <120997174+kruypisey@users.noreply.github.com>
2023-01-20 10:21:15 +0700

    upload file

---
+++ 02_KruyPisey/06_ex01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>domo php8 wiht datatype</title>
+</head>
+<body>
+    <?php
+    $age=23;
+    //var_dump($age);
+    echo -your age is:"$age;
+    echo -<or>-;
+    echo -your age is:$age-;
+    echo "your age is:$age";
+</body>
+</html>
