3cfc1ba6405ace82aad58014a35790a2fa8e156b
seak_song <120348652+SEak-Song@users.noreply.github.com>
2023-01-23 09:36:46 +0700

    Upload Lesson

---
+++ 28_SeakSong/PHP8_Demo/Lesson05/echo_print.php
@@ -0,0 +1,10 @@
+<?php
+    $txt = "Hello World";
+    $num = 123456789;
+    $colors = array("Red","Green","Blue");
+
+    echo $txt;
+    echo "<br>$num";
+    echo "<br>";
+    echo $colors[1];
+?>
